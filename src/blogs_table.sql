289898e61cbab4b23fc3e61f933988ffbad5772a
tungmv <tungminh4399@gmail.com>
2024-10-01 06:16:09 +0000

    hell world!

---
+++ src/blogs_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE blogs (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  content TEXT NOT NULL,
+  user_id INT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
